e25e07c5a976cec3793c5e459f1cc3011fef569a
TomP22 <to.penap@duocuc.cl>
2026-06-23 21:51:46 -0400

    chore: Integracion de cliente Eureka en pom y propiedades de desarrollo

--- src/main/java/cl/municipalidad/reservas/ReservasApplication.java
+++ src/main/java/cl/municipalidad/reservas/ReservasApplication.java
@@ -2,8 +2,10 @@ package cl.municipalidad.reservas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReservasApplication {
 
 	public static void main(String[] args) {
